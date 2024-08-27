57249975f73771d1dc736808b630f8c882698e90
Ethan <ethangkshim@outlook.com>
2024-08-26 20:57:50 -0400

    add tailwind and pics

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import userSeif from "./assets/user-seif.jpeg";
+import grizzly from "./assets/grizzlypng.jpeg";
+import davolaf from "./assets/davolaf.jpeg";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="w-screen h-screen flex justify-center items-center">
+      <p>Hello World</p>
     </div>
   );
 }
